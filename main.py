82191078adf72fca7250eb1309bced0c2f73c02c
João Pedro <joaopedrobdalves@gmail.com>
2026-03-17 08:53:13 -0300

    adicionando fatorial

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-n = (input("Informe um numero "))
+def fatorial_rec(n:int) -> int:
+
+
+#Caso base 
+  if (n <=1):
+    return 1 
+  else:
+   return n * fatorial_rec(n-1)
+
+
+def fatorial(n: int) -> int :
+  '''    algorotimo interativo para resover fatorial
+      input:
+         n: int - Um valor inteiro Qualquer >0
+      output:
+         result - Um valor inteiro >0
+  '''
+  
+    
+  res = 1
+  for i in range(1, n + 1):
+    res *= i
+  return res
 
 try: 
-    n=int(n)
-    print(n)
+    print("==================FATORIAL========================")
+    print("")
+    n = int(input("Informe um numero "))
+    print(f'Resultado interativo:{fatorial(n)}')
+    print(f'Resultado Recursivo:{fatorial_rec(n)}')
 
 except ValueError:
  print("Isso não e um numero")
